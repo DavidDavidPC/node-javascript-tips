82b1dab7e9fbe40059d0b0c771f1ce7a0163c6a1
David Panal Cabrera <david.panal.cabrera@everis.com>
2021-05-03 12:47:43 +0200

    Clonar objeto

--- objetos/clonar-objeto.js
+++ objetos/clonar-objeto.js
@@ -2,14 +2,70 @@
  * Cuando copiamos los valores de un objeto a otras variables, lo que estamos copiando realmente es la referencia a dicho objeto, por lo que si modificamos la copia modificamos también el original.
  */
 
- let persona = {
+let persona = {
     nombre: 'Nombre por defecto',
     edad: 0,
 }
 
-let juandc = persona
-juandc.nombre = 'Juan David'
+let davidpc = persona
+davidpc.nombre = 'David'
 
-console.log(juandc.nombre) // Juan David
+console.log(davidpc.nombre) // David
 
-console.log(persona.nombre) // Juan David ???
+console.log(persona.nombre) // David ???
+
+/**
+ * Para evitar esto se usa Object.assign
+ */
+
+let persona = {
+    nombre: 'Nombre por defecto',
+    edad: 0,
+}
+
+let davidpc = Object.assign({} , persona)
+davidpc.nombre = 'David'
+
+console.log(davidpc.nombre) // David
+
+console.log(persona.nombre) // Nombre por defecto
+
+/**
+ * Pero, ¿qué ocurre si el objeto tiene a su vez dentro más objetos? Pues que usamos JSON.parse y JSON.stringify
+ */
+
+let persona = {
+    nombre: 'Nombre por defecto',
+    edad: 0,
+    mascota: {
+            nombre: 'Mascota por defecto',
+            edad: 0,
+    }
+}
+
+let juandc = Object.assign({} , persona)
+juandc.mascota.nombre = 'Chacarron'
+
+console.log(juandc.mascota.nombre) // 'Chacarron'
+
+console.log(persona.mascota.nombre) // 'Chacarron' ???????
+
+/**
+ * Usando JSON.parse y stringify
+ */
+
+let persona = {
+    nombre: 'Nombre por defecto',
+    edad: 0,
+    mascota: {
+            nombre: 'Mascota por defecto',
+            edad: 0,
+    }
+}
+
+let juandc = JSON.parse(JSON.stringify(persona))
+juandc.mascota.nombre = 'Chacarron'
+
+console.log(juandc.mascota.nombre) // 'Chacarron'
+
+console.log(persona.mascota.nombre) // 'Mascota por defecto' ???????
