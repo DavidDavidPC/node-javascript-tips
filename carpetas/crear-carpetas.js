155be52af20d9321cca159cbf102ad1b19d40299
David Panal Cabrera <david.panal.cabrera@everis.com>
2021-05-12 12:43:10 +0200

    Http and Json methods

---
+++ carpetas/crear-carpetas.js
@@ -0,0 +1,13 @@
+// https://nodejs.dev/learn/working-with-folders-in-nodejs
+
+const fs = require('fs')
+
+const folderName = '/Users/joe/test'
+
+try {
+  if (!fs.existsSync(folderName)) {
+    fs.mkdirSync(folderName)
+  }
+} catch (err) {
+  console.error(err)
+}
