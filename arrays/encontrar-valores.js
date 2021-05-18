86e8e03cab10f451ce63a147c35d95d495dea538
David Panal Cabrera <david.panal.cabrera@everis.com>
2021-05-18 16:36:19 +0200

    Add array method

--- arrays/encontrar-valores.js
+++ arrays/encontrar-valores.js
@@ -7,3 +7,10 @@ const frutas = ['pera', 'manzana', 'naranja', 'fresa'];
 const fresa = frutas.find(fruta => fruta === 'fresa');
 
 console.log(fresa);
+
+/**
+ * Si queremos comprobar si un valor está dentro de un array
+ */
+
+ var fruits = ["Banana", "Orange", "Apple", "Mango"];
+ var n = fruits.includes("Mango");
